f309f71a3f4328fa50ad177978219bbb49f16232
BuildTools <jackisgood301@gmail.com>
2019-07-10 12:50:11 -0400

    funny comments

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 /* global localStorage, Audio */
-var beagleCount = 0
-if (localStorage.getItem('beagleSave')) {
-  beagleCount = JSON.parse(localStorage.getItem('beagleSave'))
-  document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount
+
+var beagleCount = 0 // Init the counter
+if (localStorage.getItem('beagleSave')) { // If there's a save
+  beagleCount = JSON.parse(localStorage.getItem('beagleSave')) // Change the count to the save
+  document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Show that count to the user
 }
-document.body.onclick = () => {
-  var a = new Audio('sound.mp3')
-  a.play()
-  beagleCount++
-  localStorage.setItem('beagleSave', beagleCount)
-  document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount
+
+document.body.onmouseup = () => { // When the user clicks
+  var a = new Audio('sound.mp3') // Init the sound
+  a.play() // Play that sound
+  beagleCount++ // Add to the beagle count
+  localStorage.setItem('beagleSave', beagleCount) // Save the counter
+  document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Update the displayed count
 }
