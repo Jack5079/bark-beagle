7ee370ed8586cfb77756b2b5dd7eec7aaf37efa3
BuildTools <jackisgood301@gmail.com>
2019-07-10 12:46:41 -0400

    how did i forget

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var beagleCount = 0
 if (localStorage.getItem('beagleSave')) beagleCount = JSON.parse(localStorage.getItem('beagleSave'))
 document.body.onclick = () => {
-  var a = new Audio('bruh.mp3')
+  var a = new Audio('sound.mp3')
   a.play()
   beagleCount++
   localStorage.setItem('beagleSave', beagleCount)
