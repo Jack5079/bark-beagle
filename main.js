46bf35a8627c3b5eb48c25c4d32546ba5bc21b88
BuildTools <jackisgood301@gmail.com>
2019-07-11 14:55:48 -0400

    comments

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ function inc () { // When the user clicks
   beagleCount++ // Add to the beagle count
   localStorage.setItem('beagleSave', beagleCount) // Save the counter
   document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Update the displayed count
-  document.getElementById('beagle').style.transform = 'rotate(180deg)'
-  document.getElementById('beagle').src = './bark.jpg'
+  document.getElementById('beagle').style.transform = 'rotate(180deg)' // Because the barking picture is rotated in-file, we must update the rotation to account for this.
+  document.getElementById('beagle').src = './bark.jpg' // Switch to the barking picture
   setTimeout(function () {
-    document.getElementById('beagle').style.transform = 'rotate(90deg)'
-    document.getElementById('beagle').src = './beg.jpg'
+    document.getElementById('beagle').style.transform = 'rotate(90deg)' // Revert the rotation
+    document.getElementById('beagle').src = './beg.jpg' // Switch to the begging picture (the default)
   }, 1000)
 }
