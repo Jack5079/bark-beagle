977b35171c6df4371ecf8a1b91a39cf6fe9ce4ac
BuildTools <jackisgood301@gmail.com>
2019-07-11 14:57:00 -0400

    funy bark

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-/* global localStorage, Audio */
-
 var beagleCount = 0 // Init the counter
-if (localStorage.getItem('beagleSave')) { // If there's a save
-  beagleCount = JSON.parse(localStorage.getItem('beagleSave')) // Change the count to the save
+if (window.localStorage.getItem('beagleSave')) { // If there's a save
+  beagleCount = JSON.parse(window.localStorage.getItem('beagleSave')) // Change the count to the save
   document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Show that count to the user
 }
 
 function inc () { // When the user clicks
-  var a = new Audio('sound.mp3') // Init the sound
+  var a = new window.Audio('sound.mp3') // Init the sound
   a.play() // Play that sound
   beagleCount++ // Add to the beagle count
-  localStorage.setItem('beagleSave', beagleCount) // Save the counter
+  window.localStorage.setItem('beagleSave', beagleCount) // Save the counter
   document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Update the displayed count
   document.getElementById('beagle').style.transform = 'rotate(180deg)' // Because the barking picture is rotated in-file, we must update the rotation to account for this.
   document.getElementById('beagle').src = './bark.jpg' // Switch to the barking picture
