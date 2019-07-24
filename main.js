8f56fc4f3bd255aa174062054a9a9cb33d757635
BuildTools <jackisgood301@gmail.com>
2019-07-24 16:41:43 -0400

    add funny easter egg

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-var beagleCount = 0 // Init the counter
+let beagleCount = 0 // Init the counter
 if (window.localStorage.getItem('beagleSave')) { // If there's a save
   beagleCount = JSON.parse(window.localStorage.getItem('beagleSave')) // Change the count to the save
   document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Show that count to the user
 }
 
+let randomnum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
 document.querySelector('#beagle').onclick = function () { // When the user clicks
+  if (randomnum(0, 200) === 50) {
+    let dog = document.createElement('dialog')
+    dog.innerText = 'SUPER RARE BRUH: +100 BRUHS!'
+    dog.open = true
+    dog.style.zIndex = '100'
+    document.body.appendChild(dog)
+    setTimeout(function () { dog.remove() }, 5000)
+    let a = new window.Audio('sound.mp3') // Init the sound
+    a.play() // Play that sound
+    beagleCount += 100 // Add 100 to the beagle count
+    window.localStorage.setItem('beagleSave', beagleCount) // Save the counter
+    document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Update the displayed count
+  } else {
+    let a = new window.Audio('sound.mp3') // Init the sound
+    a.play() // Play that sound
+    beagleCount++ // Add to the beagle count
+    window.localStorage.setItem('beagleSave', beagleCount) // Save the counter
+    document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Update the displayed count
+  }
   clearTimeout(window.revert)
-  var a = new window.Audio('sound.mp3') // Init the sound
-  a.play() // Play that sound
-  beagleCount++ // Add to the beagle count
-  window.localStorage.setItem('beagleSave', beagleCount) // Save the counter
-  document.getElementById('counter').innerText = 'Bruh count: ' + beagleCount // Update the displayed count
   document.getElementById('beagle').style.transform = 'rotate(180deg)' // Because the barking picture is rotated in-file, we must update the rotation to account for this.
   document.getElementById('beagle').src = './bark.jpg' // Switch to the barking picture
   window.revert = setTimeout(function () {
