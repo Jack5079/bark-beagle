dd983106d162c51499c833a1aaa136460adb0850
Jack <29169102+Jack5079@users.noreply.github.com>
2019-11-08 19:10:04 -0500

    Modularize Bruh Beagle 2. This also finally makes the last counter dependent on the DOM.

---
+++ main.mjs
@@ -0,0 +1,23 @@
+/* global localStorage, Audio */
+
+import inc from './inc.mjs'
+
+inc((+localStorage.getItem('beagleSave')) || 0) // Load the counter
+
+// Import the upgrades
+import './width.mjs'
+import './strength.mjs'
+
+
+// onclick
+document.getElementById('beagle').addEventListener('click', e => { // When the user clicks
+  new Audio('sound.mp3').play() // play a bruh sound
+  inc(1) // add 1 to the bruh count
+  clearTimeout(window.revert) // remove the timeout if it's there
+  if ((Math.floor(Math.random() * 50) + 1) === 20) { // 1 in 50 chance
+    document.getElementById('beagle').src = './bagel.jpg' // Switch to the bagel picture
+  } else document.getElementById('beagle').src = './bark.png' // Switch to the barking picture if it wasn't the bagel
+  window.revert = setTimeout(() => { // 1 second later
+    document.getElementById('beagle').src = './beg.png' // Switch to the begging picture (the default)
+  }, 1000)
+})
---
+++ strength.mjs
@@ -0,0 +1,41 @@
+import Upgrade from './upgrade.mjs'
+let BiggerBark = new class extends Upgrade { // A bigger bark.
+
+  constructor() { // Runs on load.
+    super() // MUST PUT SUPER FOR UPGRADES TO WORK
+    let strengthtml = document.createElement('span') // the html counter
+    strengthtml.id = 'strength' // add the id
+    strengthtml.innerText = localStorage.getItem('beagleStr') || 1 // load the strength
+    strengthtml.classList.add('counter') // add counter class
+    document.body.appendChild(strengthtml) // add to body
+
+    document.getElementById('beagle').addEventListener('click', () => { // Add the actual upgrade; this gives you the bigger bark.
+      inc(this.strength - 1) // add strength - 1 to the bruh count (- 1 is so we can account for the builtin onclick)
+    })
+  }
+
+  onbuy (price) { // when they buy it
+    this.price *= 1.5 // almost double the price
+    this.strength++ // add to the strength
+  }
+
+  meta () { // info about bigger bark
+    return {
+      name: 'Bigger Bark',
+      desc: 'Increases your bruh strength ( bruhs on click ) by one.',
+      startprice: 30
+    }
+  }
+
+
+  get strength () {
+    return +document.getElementById('strength').innerText // return the bruh strength innertext
+  }
+
+  set strength (amount) {
+    document.getElementById('strength').innerText = +amount // set the innertext
+    localStorage.setItem('beagleStr', +amount) // save it
+  }
+}
+
+export default BiggerBark
