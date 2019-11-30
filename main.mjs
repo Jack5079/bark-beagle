967200b4382e54f06045fe12a5fde48bfc26ba3a
Jack <29169102+Jack5079@users.noreply.github.com>
2019-11-30 13:25:40 -0500

    Change bruh timings

--- main.mjs
+++ main.mjs
@@ -15,13 +15,14 @@ import './achivements/bagel.mjs'
 
 // onclick
 document.getElementById('beagle').addEventListener('click', () => { // When the user clicks
-  new Audio('sound.mp3').play() // play a bruh sound
+  if ('aud' in window) aud.pause()
+  window.aud = new Audio('sound.mp3') // play a bruh sound
+  aud.play()
+  aud.onended = () => { // When the audio ends
+     document.getElementById('beagle').src = './beg.png' // Switch to the begging picture (the default)
+  }
   load(1) // add 1 to the bruh count
-  clearTimeout(window.revert) // remove the timeout if it's there
   if ((Math.floor(Math.random() * 500) + 1) === 20) { // 1 in 50 chance
     document.getElementById('beagle').src = './bagel.jpg' // Switch to the bagel picture
   } else document.getElementById('beagle').src = './bark.png' // Switch to the barking picture if it wasn't the bagel
-  window.revert = setTimeout(() => { // 1 second later
-    document.getElementById('beagle').src = './beg.png' // Switch to the begging picture (the default)
-  }, 1000)
 })
