6f907b9c5fe95fe2e1578d221a9ced4db0a0313c
Jack <29169102+Jack5079@users.noreply.github.com>
2019-12-06 19:07:31 -0500

    Switch to one audio

--- main.mjs
+++ main.mjs
@@ -14,16 +14,17 @@ import './upgrades/ending.mjs'
 import './achivements/firstBruh.mjs'
 import './achivements/bagel.mjs'
 
+
+let audio  = new Audio('./assets/sound.mp3') // play a bruh sound
 // onclick
 document.getElementById('beagle').addEventListener('click', () => { // When the user clicks
-  if ('aud' in window) aud.pause()
-  window.aud = new Audio('./assets/sound.mp3') // play a bruh sound
-  aud.play() // Play the sound
-  aud.onplay = () => { // When it starts
+  audio.currentTime = 0
+  audio.play() // Play the sound
+  audio.onplay = () => { // When it starts
     if ((Math.floor(Math.random() * 500) + 1) === 20) // 1 in 50 chance
      document.getElementById('beagle').src = './assets/bagel.jpg' // Switch to the bagel picture
     else document.getElementById('beagle').src = './assets/bark.png' // Switch to the barking picture if it wasn't the bagel
   }
-  aud.onended = () =>  document.getElementById('beagle').src = './assets/beg.png' // Switch to the begging picture (the default)
+  audio.onended = () =>  document.getElementById('beagle').src = './assets/beg.png' // Switch to the begging picture (the default)
   load(1) // add 1 to the bruh count
 })
