45fb1f818cd15d7074af482e4baf4138a74d7299
Jack <29169102+Jack5079@users.noreply.github.com>
2020-01-01 20:38:59 -0500

    fix first bruh

--- achivements/firstBruh.mjs
+++ achivements/firstBruh.mjs
@@ -1,5 +1,4 @@
 import Achivement from '../lib/achivement.mjs'
-import getBruh from '../lib/inc.mjs'
 import { currency } from '../config.mjs'
 const firstBruh = new Achivement(
   'First ' + currency,
@@ -7,8 +6,6 @@ const firstBruh = new Achivement(
 )
 
 document.getElementById('beagle').addEventListener('click', () => {
-  if (getBruh(0) === 0) {
     firstBruh.progress = 100
-  }
 })
 export default firstBruh
